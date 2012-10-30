aadadc4c1d392f9771ea34e8c9fa9d69384e7b9a
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-10-30 10:56:38 +0100

    +treeforall

--- src/pm/pmuse.erl
+++ src/pm/pmuse.erl
@@ -3,13 +3,12 @@
 %% Sebastian Paaske Tørholm <sebbe@diku.dk>
 
 -module(pmuse).
--export([pmmap/2%, treeforall/2
-        ]).
+-export([pmmap/2, treeforall/2]).
 
 %% Interface
 
 pmmap(Fun, List) ->
-    Mappers = init_mappers(20, Fun),
+    Mappers = init_mappers(20, fun (D, IV) -> pm:put(IV, Fun(D)) end),
     Data    = lists:map(fun(D) -> {D, pm:newVanilla()} end, List),
     send_data(Mappers, Data),
     Results = lists:map(fun({_,IV}) -> pm:get(IV) end, Data),
@@ -17,18 +16,25 @@ pmmap(Fun, List) ->
 
     Results.
 
-%treeforall(Tree, Pred) ->
-%    Mappers = init_mappers(20, Fun),
-%    Data    = 
+treeforall(Tree, Pred) ->
+    Mappers = init_mappers(20, fun(D, IV) ->
+        pm:put(IV, {ok, D}),
+        pm:put(IV, notok)
+    end),
+    List    = preorder_treewalk(Tree),
+    OurPred = fun({ok, D}) -> Pred(D);
+                 (notok)   -> true
+              end,
+    Data    = lists:map(fun(D) -> {D, pm:newPrincess(OurPred)} end, List),
+    send_data(Mappers, Data),
+    Result = try treeforall_reduce(Data) of
+                _ -> true
+             catch
+                throw:false -> false
+             end,
+    lists:foreach(fun stop_async/1, Mappers),
 
-%% synchronous communication
-
-rpc(Pid, Request) ->
-    Pid ! {self(), Request},
-    receive
-	{Pid, Response} ->
-	    Response
-    end.
+    Result.
 
 %% asynchronous communication
 
@@ -58,12 +64,23 @@ send_loop(_, _, []) -> ok;
 send_loop(Mappers, [], Data) ->
     send_loop(Mappers, Mappers, Data).
 
+preorder_treewalk(leaf) -> [];
+preorder_treewalk({node, E, L, R}) ->
+    [E | preorder_treewalk(L) ++ preorder_treewalk(R)].
+
 mapper_loop(Fun) ->
     receive
         {data, {Data, IVar}} ->
-            Res = Fun(Data),
-            pm:put(IVar, Res),
+            Fun(Data, IVar),
             mapper_loop(Fun);
         stop ->
             ok
     end.
+
+treeforall_reduce(Data) ->
+    lists:foreach(fun({_,IV}) ->
+        case pm:get(IV) of
+            {ok, _} -> true;
+            notok   -> throw(false)
+        end
+    end, Data).
