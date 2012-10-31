daf417b665aa2372063189be196e014a85187e9a
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-10-31 13:21:01 +0100

    Parser

--- src/soil/SoilParser.hs
+++ src/soil/SoilParser.hs
@@ -1,12 +1,160 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# LANGUAGE TupleSections #-}
 --
--- Skeleton for Soil parser
--- To be used at the exam for Advanced Programming, B1-2012
+-- Soil parser
+-- Exam for Advanced Programming, B1-2012
+-- Sebastian Paaske Tørholm <sebbe@diku.dk>
 --
 
-module SoilParser where
+module SoilParser
+  ( parseString
+  , parseFile
+  , Error(..)
+  ) where
 
 import SoilAst
+import Control.Monad (ap, MonadPlus(mzero, mplus), liftM)
+import Control.Applicative ((<$>), Applicative(..),
+                            Alternative(empty, (<|>)))
+import Text.ParserCombinators.ReadP
+import Data.Char (isAlphaNum, isAlpha)
+
+-- Instances for Applicative ReadP and Alternative ReadP aren't in base-4.5
+instance Applicative ReadP where
+    pure = return
+    (<*>) = ap
+
+instance Alternative ReadP where
+    empty = mzero
+    (<|>) = mplus
+
+{- AmbiguousParseTree - there are multiple possible interpretations of the
+                        code.
+   InvalidProgram     - there's a syntax error. -}
+
+data Error = AmbiguousParseTree
+           | InvalidProgram
+    deriving (Show, Eq)
+
+parenthesized :: ReadP a -> ReadP a
+parenthesized = between (schar '(') (schar ')')
+
+-- Char preceded by whitespace
+schar :: Char -> ReadP Char
+schar c = skipSpaces >> char c
+
+-- String preceded by whitespace
+sstring :: String -> ReadP String
+sstring s = skipSpaces >> string s
+
+keyword :: String -> Bool
+keyword = flip elem ["let", "from", "end", "case", "of", "if", "then", "else",
+                     "send", "create", "become", "self"]
+
+program :: ReadP Program
+program = do d <- defops
+             skipSpaces
+             eof
+             return d
+
+defops :: ReadP Program
+defops = (do f <- fundef
+             (fs, as) <- defops
+             return (f:fs, as))
+     <|> do as <- actops
+            return ([], as)
+
+fundef :: ReadP Func
+fundef = do sstring "let"
+            i    <- ident
+            ps   <- parenthesized pars
+            sstring "from"
+            n    <- name
+            schar '='
+            ex   <- expr
+            sstring "end"
+            return $ Func i ps n ex
+
+pars :: ReadP [Name]
+pars = sepBy name (schar ',')
+
+expr :: ReadP Expr
+expr = fmap Acts actops
+   <|> (do sstring "case"
+           p <- prim
+           sstring "of"
+           (cs, def) <- cases
+           sstring "end"
+           return $ CaseOf p cs def)
+   <|> do sstring "if"
+          p1 <- prim
+          sstring "=="
+          p2 <- prim
+          sstring "then"
+          ethen <- expr
+          sstring "else"
+          eelse <- expr
+          sstring "end"
+          return $ IfEq p1 p2 ethen eelse
+
+cases :: ReadP ([([Name], Expr)], Expr)
+cases = (do ps <- parenthesized pars
+            schar ':'
+            e  <- expr
+            (cs, def) <- cases
+            return ((ps, e) : cs, def))
+    <|> ([],) <$> (schar '_' >> schar ':' >> expr)
+
+actops :: ReadP [ActOp]
+actops = many actop
+
+actop :: ReadP ActOp
+actop = (do sstring "send"
+            as <- parenthesized args
+            sstring "to"
+            p  <- prim
+            return $ SendTo as p)
+    <|> (do sstring "create"
+            p  <- prim
+            sstring "with"
+            (fcp, fca) <- fcall
+            return $ Create p fcp fca)
+    <|> do sstring "become"
+           (fcp, fca) <- fcall
+           return $ Become fcp fca
+
+fcall :: ReadP (Prim, [Prim])
+fcall = do p <- prim
+           a <- parenthesized args
+           return (p, a)
+
+args :: ReadP [Prim]
+args = skipSpaces >> sepBy prim (schar ',')
+
+prim :: ReadP Prim
+prim = chainl1 primBasic (sstring "concat" >> return Concat)
+
+primBasic :: ReadP Prim
+primBasic = fmap Id ident
+        <|> fmap Par name
+        <|> (sstring "self" >> return Self)
+
+ident :: ReadP Ident
+ident = schar '#' >> munch1((||) <$> isAlphaNum <*> (==) '_')
+
+name :: ReadP Name
+name = do skipSpaces
+          n  <- satisfy isAlpha
+          ns <- munch((||) <$> isAlphaNum <*> (==) '_')
+          case n:ns of
+            name' | keyword name' -> pfail
+                  | otherwise     -> return name'
 
 parseString :: String -> Either Error Program
+parseString s = case readP_to_S program s of
+                    []       -> Left InvalidProgram
+                    [(p,"")] -> Right p
+                    _        -> Left AmbiguousParseTree
 
 parseFile :: FilePath -> IO (Either Error Program)
+parseFile = liftM parseString . readFile
---
+++ src/soil/SoilParserTest.hs
@@ -0,0 +1,142 @@
+import SoilParser
+import SoilAst
+import Test.HUnit
+import System.FilePath (replaceExtension, replaceBaseName)
+
+-- ASTs {{{
+helloWorldAST :: Program
+helloWorldAST =
+    (
+        [Func {
+            funcname = "print",
+            params = [],
+            receive = "message",
+            body = Acts [SendTo [Par "message"] (Id "println")]}
+        ],
+        [Create (Id "hw1") (Id "print") []
+        ,Create (Id "hw2") (Id "print") []
+        ,SendTo [Id "Hello"] (Id "hw1")
+        ,SendTo [Id "World"] (Id "hw2")
+        ]
+    )
+
+cleanUpAST :: Program
+cleanUpAST =
+    (
+        [Func {
+            funcname = "dub",
+            params = [],
+            receive = "message",
+            body = CaseOf (Par "message") [
+                (["sender", "msg"]
+                    ,Acts [
+                        SendTo [Self, Par "msg"] (Par "sender"),
+                        SendTo [Self, Par "msg"] (Par "sender")]
+                )]
+                (Acts [SendTo [Id "FaultyMessage"] (Id "println")])
+            }
+        ,Func {
+            funcname = "half",
+            params = ["state"],
+            receive = "message",
+            body = IfEq (Par "state") (Id "skip")
+                (Acts [
+                        Become (Id "half") [Id "return"],
+                        SendTo [Id "SkippingMessage"] (Id "println")]
+                )
+                (CaseOf (Par "message") [
+                    (["sender", "msg"]
+                        ,Acts [
+                            Become (Id "half") [Id "skip"],
+                            SendTo [Self, Par "msg"] (Par "sender")]
+                    )]
+                    (Acts [SendTo [Id "FaultyMessage"] (Id "println")])
+                )
+            }
+        ],
+        [Create (Id "dubproc") (Id "dub") []
+        ,Create (Id "halfproc") (Id "half") [Id "return"]
+        ,SendTo [Id "halfproc", Id "foo"] (Id "dubproc")
+        ]
+    )
+
+gateKeeperAST :: Program
+gateKeeperAST =
+    (
+        [Func {
+            funcname = "printer",
+            params = [],
+            receive = "message",
+            body = Acts [SendTo [Par "message"] (Id "println")]
+            }
+        ,Func {
+            funcname = "gate",
+            params = ["fst", "fstmsg"],
+            receive = "message",
+            body = CaseOf (Par "message") [
+                (["snd", "sndmsg"]
+                    , IfEq (Par "fst") (Id "none")
+                        (Acts [Become (Id "gate") [Par "snd", Par "sndmsg"]])
+                        (Acts [
+                            SendTo [Par "fstmsg", Par "sndmsg"]
+                                (Concat (Par "fst") (Par "snd"))
+                            ,
+                            Become (Id "gate") [Id "none", Id "none"]]
+                        )
+                )]
+                (Acts [])
+            }
+        ,Func {
+            funcname = "repeat",
+            params = ["other"],
+            receive = "message",
+            body = Acts [
+                SendTo [Par "message"] (Id "gatekeeper"),
+                SendTo [Par "message"] (Par "other")]
+            }
+        ],
+        [Create (Id "foobar") (Id "printer") []
+        ,Create (Id "gatekeeper") (Id "gate") [Id "none", Id "none"]
+        ,Create (Id "repeater1") (Id "repeat") [Id "repeater2"]
+        ,Create (Id "repeater2") (Id "repeat") [Id "repeater1"]
+        ,SendTo [Id "foo", Id "Hello"] (Id "repeater1")
+        ,SendTo [Id "bar", Id "World"] (Id "repeater2")
+        ,SendTo [Id "foo", Id "Bye"] (Id "repeater1")
+        ]
+    )
+-- }}} end of ASTs
+
+-- Test cases that were handed out
+fileTestCases :: [(String, Program)]
+fileTestCases = [("helloWorld", helloWorldAST),
+             ("cleanUp", cleanUpAST),
+             ("gateKeeper", gateKeeperAST)]
+
+testWithFile :: (String, Program) -> [IO Test]
+testWithFile (f, ast) =
+    [ do got <- parseFile filePath
+         return $ TestCase $ assertEqual ("parseFile (" ++ f ++ ")")
+                                         (Right ast) got
+    , do src <- readFile filePath
+         return $ TestCase $ assertEqual ("parseString (" ++ f ++ ")")
+                                         (Right ast) $ parseString src
+    ]
+    where file = replaceExtension f ".soil"
+          filePath = replaceBaseName "examples/" file
+
+invalidProgramTest :: IO Test
+invalidProgramTest = return $ TestCase
+                            $ assertEqual "invalid program"
+                              (Left InvalidProgram)
+                            $ parseString "case foo of _ : end"
+
+
+fileTests :: IO Test
+fileTests = fmap TestList $ sequence $ concatMap testWithFile fileTestCases
+
+tests :: IO Test
+tests = fmap TestList $ sequence [fileTests, invalidProgramTest]
+
+main :: IO Counts
+main = do t <- tests
+          runTestTT t
